b2693b61167fc790ad8994552913851eb9ad0c3a
ac009527 <achaunweb@gmail.com>
2024-07-09 15:50:53 +0800

    getBalance

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import "./i18n";
 import { useTranslation } from "react-i18next";
 import styles from "./App.module.scss";
-import { createWalletClient, http, PublicClient } from 'viem';
+import { createWalletClient, http, PublicClient, publicActions, walletActions } from 'viem';
 import { mainnet } from 'viem/chains';
 import { privateKeyToAccount } from "viem/accounts";
+import { mnemonicToAccount } from 'viem/accounts'
+
 function App() {
   const { t } = useTranslation();
   const clientRef = useRef<PublicClient>(null) as any; // 我的钱包客户端
   useEffect(() => {
+    //  生成助记词方法 const mnemonic = generateMnemonic(english)
 
-      // 生成账户
-      const account = privateKeyToAccount("0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
-      const client = createWalletClient({
-        account,
-        chain: mainnet,
-        transport: http()
-      })
-      clientRef.current = client;
+    // 通过助记词生成账户
+    const account = mnemonicToAccount('legal winner thank year wave sausage worth useful legal winner thank wow')
+    // const account = mnemonicToAccount('legal winner thank year wave sausage worth useful legal winner thank yellow')
+
+    // 私钥生成账户
+    // const account = privateKeyToAccount("0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef");
+    const client = createWalletClient({
+      account,
+      chain: mainnet,
+      transport: http()
+    })
+      .extend(publicActions)
+      .extend(walletActions)
+    // 获取余额
+    client.getBalance({
+      address: account.address,
+    }).then((balance) => {
+      console.log('balance', balance.toString());
+    })
+    clientRef.current = client;
 
   }, [])
   return (
